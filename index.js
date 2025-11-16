5ecad2df1d8b608a938688e410cb71214b458388
spy-1234 <janmejaisinghvasu20@gmail.com>
2025-11-17 01:34:43 +0530

    schemasnmodels

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-const express = require('express')
-require('dotenv').config()
-const mongoose = require('mongoose')
+require("dotenv").config();
 
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
 
-const PORT = process.env.MONGO_URI || 3001
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.listen(PORT, ()=> {
-    console.log("Server is running")
-    mongoose.connect(PORT)
-    console.log("Connected to MongoDB")
-})
+const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
+
+
+const PORT = process.env.MONGO_URL || 3002;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
+});
+
+app.listen(PORT, () => {
+  console.log("App started!");
+  mongoose.connect(PORT);
+  console.log("DB started!");
+});
